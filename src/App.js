453cc10ee1fa114d7180009f67540cb0e5923098
suprajanew2023 <bbfreesupraja@gmail.com>
2023-12-02 09:57:42 +0530

    added dashboard code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import PieChart from './components/PieChart'
+import Status from './components/Status'
+import Dashboard from './components/Dashboard';
+import ExpenseApproval from './components/ExpenseApproval'
 function App() {
   return (
   <>
-<PieChart/>
+{/* <PieChart/> */}
+{/* <Status/> */}
+<Dashboard/>
+{/* <ExpenseApproval/> */}
   </>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, Typography } from '@mui/material';
+import PieChart from './PieChart';
+import Barchart from './Barchart';
+import DonutChart from './DonutChart';
+import axios from 'axios';
+
+const Dashboard = () => {
+  const [currentMonthCost, setCurrentMonthCost] = useState(null);
+  const [lastMonthCost, setLastMonthCost] = useState(null);
+
+  useEffect(() => {
+    const fetchCostData = async () => {
+      try {
+        const currentMonthResponse = await axios.get('http://localhost:8000/currentMonthCosts');
+        setCurrentMonthCost(currentMonthResponse.data.totalCosts);
+
+        const lastMonthResponse = await axios.get('http://localhost:8000/lastMonthCosts');
+        setLastMonthCost(lastMonthResponse.data.totalCosts);
+      } catch (error) {
+        console.error('Error fetching cost data:', error);
+      }
+    };
+
+    fetchCostData();
+  }, []);
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h4">Expenses This Month</Typography>
+        <Typography>{currentMonthCost ? `${currentMonthCost}` : 'Loading...'}</Typography>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h4">Expenses Last Month</Typography>
+        <Typography>{lastMonthCost ? `${lastMonthCost}` : 'Loading...'}</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <PieChart />
+      </Grid>
+      <Grid item xs={12}>
+        <Barchart />
+      </Grid>
+      <Grid item xs={12}>
+        <DonutChart />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Dashboard;
